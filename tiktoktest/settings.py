d9f3fa990472c64c3df4d9d92644249f6b5dbb49
Davi Rios <manueldavisilveirarios123@gmail.com>
2026-02-20 14:57:09 -0300

    host banco de dados online

--- tiktoktest/settings.py
+++ tiktoktest/settings.py
@@ -80,16 +80,14 @@ WSGI_APPLICATION = 'tiktoktest.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'testetiktok',
-        'USER': 'django_user',
-        'PASSWORD': 'senha123',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        }
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+        "OPTIONS": {"ssl": {"ssl": {}}}
     }
 }
 
